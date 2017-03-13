b9375e4b44b8bfe6e46f32c6bc24eb53f8ac08d0
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2017-03-13 11:10:30 +0530

    Prasenjit | removed redundant messages

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -2,39 +2,33 @@ module LogAnalysis where
 import Log
 import Data.List
 import Data.List.Split
-import Data.Map (Map)
+import Data.Map ()
 import qualified Data.Map as Map
-import System.IO.Unsafe
-import Debug.Trace
 
 readValFromList :: [String] -> Int -> Int
 readValFromList inputList position = read $ head $ drop position inputList
 
 parseMessage :: String -> LogMessage
 parseMessage aLine
-  |any (\x -> (==) x messageType) ["I", "W"] =
-    LogMessage (types Map.! messageType) (readValFromList splittedLine 1) (intercalate " " (drop 2 splittedLine))
+  |messageType `elem` ["I", "W"] =
+    LogMessage (types Map.! messageType) (readValFromList splittedLine 1) (unwords (drop 2 splittedLine))
   |(==) "E" $ head splittedLine =
-    LogMessage (Error $ readValFromList splittedLine 1) (readValFromList splittedLine 2) (intercalate " " (drop 3 splittedLine))
+    LogMessage (Error $ readValFromList splittedLine 1) (readValFromList splittedLine 2) (unwords (drop 3 splittedLine))
   |otherwise = Unknown aLine
   where splittedLine = splitOn " " aLine
         types = Map.fromList [("I", Info), ("W", Warning)]
         messageType = head splittedLine
 
 parse :: String -> [LogMessage]
-parse fileString
-  |otherwise = map parseMessage linesOfFile
+parse fileString = map parseMessage linesOfFile
   where linesOfFile = lines fileString
 
-testParse :: (String -> [LogMessage]) -> Int -> String -> [LogMessage]
-testParse parseFunc buffer filePath = take buffer $ parse (unsafePerformIO . readFile $ filePath)
-
 insertInto :: MessageTree -> LogMessage -> MessageTree
-insertInto tree (Unknown message) = tree
+insertInto tree (Unknown _) = tree
 insertInto Leaf x = Node Leaf x Leaf
 insertInto (Node t1 (LogMessage a b c) t2) (LogMessage p q r)
-	| (<) b q = Node t1 (LogMessage a b c) (insertInto t2 (LogMessage p q r))
-	| otherwise = Node (insertInto t1 (LogMessage p q r)) (LogMessage a b c) t2
+ |(<) b q = Node t1 (LogMessage a b c) (insertInto t2 (LogMessage p q r))
+ |otherwise = Node (insertInto t1 (LogMessage p q r)) (LogMessage a b c) t2
 
 build :: [LogMessage] -> MessageTree
 build = foldl insertInto Leaf
@@ -47,10 +41,10 @@ inOrder Leaf = []
 inOrder (Node t1 x t2) = inOrder t1 ++ [x] ++ inOrder t2
 
 relevantErrorMessage :: LogMessage -> String
-relevantErrorMessage (Unknown message) = ""
-relevantErrorMessage (LogMessage Info timeStamp message) = ""
-relevantErrorMessage (LogMessage Warning timeStamp message) = ""
-relevantErrorMessage (LogMessage (Error siviarity) timeStamp message)
+relevantErrorMessage (Unknown _) = ""
+relevantErrorMessage (LogMessage Info _ _) = ""
+relevantErrorMessage (LogMessage Warning _ _) = ""
+relevantErrorMessage (LogMessage (Error siviarity) _ message)
   | (>=) siviarity 50 = message
   | otherwise = ""
 
--- LogAnalysisTest.hs
+++ LogAnalysisTest.hs
@@ -1,7 +1,7 @@
 module LogAnalysisTest where
   import Test.Hspec
-  import Test.QuickCheck
-  import Control.Exception (evaluate)
+  import Test.QuickCheck()
+  import Control.Exception()
   import LogAnalysis
   import Log
 
@@ -21,51 +21,49 @@ module LogAnalysisTest where
                   "I 9 Back from lunch"
 
     describe "parseMessage" $ do
-      it "should return   LogMessage (Error 2) 562 help help for E 2 562 help help" $ do
+      it "should return   LogMessage (Error 2) 562 help help for E 2 562 help help" $
         parseMessage "E 2 562 help help" `shouldBe`  LogMessage (Error 2) 562 "help help"
 
-      it "should return LogMessage Info 29 la la la for I 29 la la la" $ do
+      it "should return LogMessage Info 29 la la la for I 29 la la la" $
         parseMessage "I 29 la la la" `shouldBe` LogMessage Info 29 "la la la"
 
-      it "should return Unknown This is not in the right format for This is not in the right format" $ do
+      it "should return Unknown This is not in the right format for This is not in the right format" $
         parseMessage "This is not in the right format" `shouldBe` Unknown "This is not in the right format"
 
-      it "should return Warning 5 Flange is due for a check-up for W 5 Flange is due for a check-up" $ do
+      it "should return Warning 5 Flange is due for a check-up for W 5 Flange is due for a check-up" $
         parseMessage "W 5 Flange is due for a check-up" `shouldBe` LogMessage Warning 5 "Flange is due for a check-up"
 
-    describe "parse" $ do
+    describe "parse" $
 
-      it "should create Logmessage for the given string" $ do
+      it "should create Logmessage for the given string" $
         parse "E 2 562 help help\nI 29 la la la" `shouldBe` [LogMessage (Error 2) 562 "help help", LogMessage Info 29 "la la la"]
 
     describe "insertInto" $ do
-      it "should create a new tree when given a empty tree and a Logmessage" $ do
-        insertInto Leaf (LogMessage (Error 2) 562 "help help") `shouldBe` (Node Leaf (LogMessage (Error 2) 562 "help help") Leaf)
+      it "should create a new tree when given a empty tree and a Logmessage" $
+        insertInto Leaf (LogMessage (Error 2) 562 "help help") `shouldBe` Node Leaf (LogMessage (Error 2) 562 "help help") Leaf
 
-      it "should return the tree if it is given an unknown" $ do
+      it "should return the tree if it is given an unknown" $
         insertInto Leaf (Unknown "This is not in the right format") `shouldBe` Leaf
 
-      it "should insert logmessage in the left subtree if the node timestamp is greater than that of the new logmessage" $ do
+      it "should insert logmessage in the left subtree if the node timestamp is greater than that of the new logmessage" $
         insertInto (Node Leaf (LogMessage (Error 2) 562 "help help") Leaf) (LogMessage Info 540 "All is well")
-          `shouldBe` (Node (Node Leaf (LogMessage Info 540 "All is well") Leaf) (LogMessage (Error 2) 562 "help help") Leaf)
+          `shouldBe` Node (Node Leaf (LogMessage Info 540 "All is well") Leaf) (LogMessage (Error 2) 562 "help help") Leaf
 
-      it "should insert logmessage in the right subtree if the node timestamp is less than that of the new logmessage" $ do
+      it "should insert logmessage in the right subtree if the node timestamp is less than that of the new logmessage" $
         insertInto (Node Leaf (LogMessage (Error 2) 562 "help help") Leaf) (LogMessage Info 999 "All is well")
-          `shouldBe` (Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf))
+          `shouldBe` Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf)
 
       describe "build" $ do
-        it "should give Leaf for empty list" $ do
+        it "should give Leaf for empty list" $
           build [] `shouldBe` Leaf
 
-        it "should build the tree with the given list of log messages" $ do
-          build [LogMessage (Error 2) 562 "help help", LogMessage Info 999 "All is well"]
-          `shouldBe` (Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf))
+        it "should build the tree with the given list of log messages" $
+          build [LogMessage (Error 2) 562 "help help", LogMessage Info 999 "All is well"] `shouldBe` Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf)
 
-      describe "inOrder" $ do
-        it "should traverse the tree and give a list of log messages" $ do
-          inOrder (Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf))
-          `shouldBe` [LogMessage (Error 2) 562 "help help", LogMessage Info 999 "All is well"]
+      describe "inOrder" $
+        it "should traverse the tree and give a list of log messages" $
+          inOrder (Node Leaf (LogMessage (Error 2) 562 "help help") (Node Leaf (LogMessage Info 999 "All is well") Leaf)) `shouldBe` [LogMessage (Error 2) 562 "help help", LogMessage Info 999 "All is well"]
 
-      describe "whatWentWrong" $ do
-        it "should give the relevant log messages from a list of message" $ do
+      describe "whatWentWrong" $
+        it "should give the relevant log messages from a list of message" $
           whatWentWrong (parse fileString) `shouldBe` ["Flange failed!", "Way too many pickles", "Bad pickle-flange interaction detected"]
